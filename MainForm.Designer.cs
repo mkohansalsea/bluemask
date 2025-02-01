ec2e5f69e0fdf12fc2a767618ecbff414cb0d041
mkohansalsea <m.kohansal.sea@gmail.com>
2025-02-01 14:07:38 +0330

    Create MainForm.Designer.cs

---
+++ MainForm.Designer.cs
@@ -0,0 +1,15 @@
+namespace BlueMask
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "MainForm";
+        }
+    }
+}
